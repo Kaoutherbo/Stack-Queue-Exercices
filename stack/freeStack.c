8156695350c6c8d5918b16ac245bbaa3f052f3ce
kaouther <kaoutherboutheldja@gmail.com>
2023-11-05 23:08:52 +0100

    add files

--- stack/freeStack.c
+++ stack/freeStack.c
@@ -1,5 +1,11 @@
 #include "stack.h"
 
+/**
+ * freeStack - function to free the stack
+ * 
+ * @head: pointer to pointer of the head element in stack
+ * Return: pointer to NULL
+*/
 StackNode* freeStack(StackNode** head) {
     while (*head != NULL) {
         *head = pop(*head);
