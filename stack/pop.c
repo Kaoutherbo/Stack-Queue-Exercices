329b9228dea8609f651f7559d203f3ff4c84c80b
kaouther <kaoutherboutheldja@gmail.com>
2023-11-05 22:31:12 +0100

    add files

--- stack/pop.c
+++ stack/pop.c
@@ -1,13 +1,16 @@
 #include "stack.h"
+/**
+ * pop - function that delete the first element in stack (summit)
+ * 
+ * @**head: pointer to the head pointer in the stack 
+ * Return: pointer to the new head pointer after deleting the first element
+ * */ 
 
-// Delete in the beginning  
-
-StackNode* pop(StackNode *head){
-    if(head != NULL)
-    {
-        StackNode* oldHead = head;
-        head = head->next;
+StackNode* pop(StackNode** head) {
+    if (*head != NULL) {
+        StackNode* oldHead = *head;
+        *head = (*head)->next;
         free(oldHead);
     }
-    return head;
+    return *head;
 }
