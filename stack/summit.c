ac0b0424b8c94287a7439c0f1cba85c743dca40a
kaouther <kaoutherboutheldja@gmail.com>
2023-11-05 22:41:45 +0100

    add files

--- stack/summit.c
+++ stack/summit.c
@@ -1,6 +1,12 @@
 #include "stack.h"
 
-// Get the first element in the satck
+/**
+ * summit - Get the data of the first element in the satck
+ * 
+ * @head: pointer to the first element in the satck
+ * Return: an integer of the first element in the stack
+*/
+ 
 int summit(StackNode *head)
 {
     return head->data;
