ecfa966811aa8d5dae75b4725f33ed4dc6658339
kaouther <kaoutherboutheldja@gmail.com>
2023-11-13 23:54:58 +0100

    Add changes

--- stack/stack.c
+++ stack/stack.c
@@ -33,12 +33,12 @@ int main() {
 
    // int size1 = getSize("How many elements you want in the stack 1 ? : ");
     
-    //int size2 = getSize("How many elements you want in the stack 2 ? : ");
+    // int size2 = getSize("How many elements you want in the stack 2 ? : ");
 
-    /*printf("Enter the elements of first stack : \n");
-    StackNode* stack1 = createStack(size1);//*evenStack, *oddStack;
-   /* printf("Enter the elements of second stack : \n");
-    StackNode* stack2 = createStack(size2);
+     // printf("Enter the elements of first stack : \n");
+    // StackNode* stack1 = createStack(size1, *evenStack, *oddStack);
+    // printf("Enter the elements of second stack : \n");
+    // StackNode* stack2 = createStack(size2);
 
     // Check the two stacks if they're identitic or not
     
